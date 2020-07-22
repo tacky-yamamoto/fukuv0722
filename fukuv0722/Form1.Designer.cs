69e602420adff1442f35b910a0b31fb4b69f7941
A601-22 <tnk.dat.class@gmail.com>
2020-07-22 13:30:18 +0900

    変数の定義

--- fukuv0722/Form1.Designer.cs
+++ fukuv0722/Form1.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -48,24 +46,6 @@
             this.label1.Size = new System.Drawing.Size(57, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 495);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 19);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "0";
-            this.textBox1.Visible = false;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 470);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 19);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Text = "0";
-            this.textBox2.Visible = false;
             // 
             // timer1
             // 
@@ -125,8 +105,6 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -138,8 +116,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- fukuv0722/Form1.cs
+++ fukuv0722/Form1.cs
@@ -12,6 +12,10 @@ namespace fukuv0722
 {
     public partial class Form1 : Form
     {
+
+        int vx = 0;
+        int vy = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,32 +23,32 @@ namespace fukuv0722
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Top += int.Parse(textBox1.Text);
-            label1.Left += int.Parse(textBox2.Text);
+            label1.Top += vx;
+            label1.Left += vy;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-1";
-            textBox2.Text = "0";
+            vx = -1;
+            vy = 0;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "-1";
+            vx = 0;
+            vy = -1;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "1";
+            vx = 0;
+            vy = 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "1";
-            textBox2.Text = "0";
+            vx = 1;
+            vy = 0;
         }
     }
 }
